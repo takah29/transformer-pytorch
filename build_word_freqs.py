92589d8f61268d91df11d923d94510e91df380d4
takah29 <takah0209@gmail.com>
2022-11-11 02:10:17 +0900

    単語頻度ファイルを作るようにした。ファイル名変更

--- build_word_freqs.py
+++ build_word_freqs.py
@@ -5,19 +5,14 @@ from collections import Counter, OrderedDict
 import json
 import itertools
 
-from torchtext.vocab import vocab
-
 from dataset import get_tokenized_text_list
 
 
-def create_vocab(file_path: Path) -> dict:
+def create_word_freqs(file_path: Path) -> dict:
     tokenized_text_list = get_tokenized_text_list(file_path)
     counter = Counter(list(itertools.chain.from_iterable(tokenized_text_list)))
 
-    return vocab(
-        OrderedDict(sorted(counter.items(), key=lambda x: x[1], reverse=True)),
-        specials=["<pad>", "<unk>", "<bos>", "<eos>"],
-    )
+    return OrderedDict(sorted(counter.items(), key=lambda x: x[1], reverse=True))
 
 
 def main():
@@ -37,13 +32,13 @@ def main():
     dataset_path = base_path / "dataset" / "small_parallel_enja-master"
 
     # 単語ID辞書をjson形式で保存する
-    vocab_ja = create_vocab(dataset_path / "train.ja")
-    with (base_path / "word_id_dict_ja.json").open("w") as f:
-        json.dump(vocab_ja.get_stoi(), f, indent=2, ensure_ascii=False)
+    word_freqs_ja = create_word_freqs(dataset_path / "train.ja")
+    with (base_path / "word_freqs_ja.json").open("w") as f:
+        json.dump(word_freqs_ja, f, indent=2, ensure_ascii=False)
 
-    vocab_en = create_vocab(dataset_path / "train.en")
-    with (base_path / "word_id_dict_en.json").open("w") as f:
-        json.dump(vocab_en.get_stoi(), f, indent=2, ensure_ascii=False)
+    word_freqs_en = create_word_freqs(dataset_path / "train.en")
+    with (base_path / "word_freqs_en.json").open("w") as f:
+        json.dump(word_freqs_en, f, indent=2, ensure_ascii=False)
 
 
 if __name__ == "__main__":
