3b9784f47b14da9953c08a81894e71257e45814f
takah29 <takah0209@gmail.com>
2022-11-21 21:33:53 +0900

    修正

--- libs/translation_model_trainer.py
+++ libs/translation_model_trainer.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torch import nn, optim
 import torch.nn.functional as F
 from torch.optim.lr_scheduler import _LRScheduler
@@ -31,7 +31,15 @@ class TransformerLRScheduler(_LRScheduler):
 
 
 class TranslationModelTrainer:
-    def __init__(self, model, optimizer, lr_scheduler, device, train_dataset, valid_dataset=None):
+    def __init__(
+        self,
+        model,
+        optimizer,
+        lr_scheduler,
+        device,
+        train_dataset,
+        valid_dataset=None,
+    ):
         self._device = device
         self._model = model.to(self._device)
 
@@ -45,26 +53,28 @@ class TranslationModelTrainer:
         self._criterion = nn.CrossEntropyLoss(ignore_index=self._train_dataset.get_pad_id()[0])
 
     def fit(self, batch_size, num_epoch):
+        train_loss = valid_loss = None
+
         train_data_loader = DataLoader(
             self._train_dataset, batch_size=batch_size, shuffle=True, num_workers=4
         )
-        train_loss = None
         train_loss_list = []
 
         if self._valid_dataset is not None:
             valid_data_loader = DataLoader(
                 self._valid_dataset, batch_size=batch_size, shuffle=True, num_workers=4
             )
-            valid_loss = None
             valid_loss_list = []
 
-        self._model.train()
         for i in range(num_epoch):
             print("epoch: ", i + 1)
+
+            self._model.train()
             train_loss = self._run_epoch(train_data_loader, is_train=True)
             train_loss_list.append(train_loss)
 
             if self._valid_dataset is not None:
+                self._model.eval()
                 valid_loss = self._run_epoch(valid_data_loader, is_train=True)
                 valid_loss_list.append(valid_loss)
 
@@ -89,6 +99,7 @@ class TranslationModelTrainer:
             )
             t = batch["dec_target"].to(self._device)
 
+            # print(y.argmax(-1).shape, t.shape)
             loss = self._criterion(y.reshape(-1, y.shape[-1]), t.reshape(-1))
             s += loss.item()
 
@@ -121,16 +132,17 @@ if __name__ == "__main__":
     from transformer import Transformer
 
     # 事前にbuild_word_freqs.pyを実行してデータセットのダウンロードと頻度辞書の作成を行っておく
-    en_txt_file_path = Path("../dataset/small_parallel_enja-master/train.en").resolve()
-    ja_txt_file_path = Path("../dataset/small_parallel_enja-master/train.ja").resolve()
+    base_path = Path("../dataset/small_parallel_enja-master").resolve()
+    en_txt_file_path = base_path / "train.en"
+    ja_txt_file_path = base_path / "train.ja"
     en_word_freqs_path = Path("../word_freqs_en.json").resolve()
     ja_word_freqs_path = Path("../word_freqs_ja.json").resolve()
     train_dataset = TextPairDataset.create(
         en_txt_file_path, ja_txt_file_path, en_word_freqs_path, ja_word_freqs_path
     )
 
-    en_val_txt_file_path = Path("../dataset/small_parallel_enja-master/dev.en").resolve()
-    ja_val_txt_file_path = Path("../dataset/small_parallel_enja-master/dev.ja").resolve()
+    en_val_txt_file_path = base_path / "dev.en"
+    ja_val_txt_file_path = base_path / "dev.ja"
     valid_dataset = TextPairDataset.create(
         en_val_txt_file_path, ja_val_txt_file_path, en_word_freqs_path, ja_word_freqs_path
     )
@@ -144,6 +156,7 @@ if __name__ == "__main__":
         "n_enc_blocks": 2,
         "n_dec_blocks": 2,
         "head_num": 8,
+        "dropout_rate": 0.1,
     }
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -151,7 +164,7 @@ if __name__ == "__main__":
     translation_model_trainer = TranslationModelTrainer(
         model, optimizer, None, device, train_dataset, valid_dataset
     )
-    train_loss_list, valid_loss_list = translation_model_trainer.fit(batch_size=128, num_epoch=15)
+    train_loss_list, valid_loss_list = translation_model_trainer.fit(batch_size=128, num_epoch=10)
 
     plt.plot(train_loss_list)
     plt.plot(valid_loss_list)
