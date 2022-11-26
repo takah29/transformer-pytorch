84c824198412cd2b30da76bf613aab1d1c77d620
takah29 <takah0209@gmail.com>
2022-11-26 22:30:12 +0900

    翻訳用の損失関数クラスを追加

--- libs/translation_model_trainer.py
+++ libs/translation_model_trainer.py
@@ -7,6 +7,18 @@ import torch.nn.functional as F
 from torch.optim.lr_scheduler import _LRScheduler
 
 
+class TranslationLoss(nn.Module):
+    def __init__(self, pad_id, label_smoothing=0.0):
+        super().__init__()
+        self.loss_func = nn.CrossEntropyLoss(ignore_index=pad_id, label_smoothing=label_smoothing)
+
+    def forward(self, logits, labels):
+        vocab_size = logits.shape[-1]
+        logits = logits.reshape(-1, vocab_size)
+        labels = labels.reshape(-1).long()
+        return self.loss_func(logits, labels)
+
+
 class TransformerLRScheduler(_LRScheduler):
     def __init__(self, optimizer, n_dim, warmup_steps):
         self._n_dim = n_dim
